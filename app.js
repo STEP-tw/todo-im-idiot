8b62c436847d3d749ecca8ec1a1532002471904b
salman shaik <dukesalman5@gmail.com>
2018-01-12 14:15:41 +0530

    handled login page

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 let webApp=require('./webapp.js');
+const fs=require('fs');
+let getFileData=require('./lib/utils.js').getFileData;
+let getMIMEType=require('./lib/utils.js').getMIMEType;
 let app=webApp.create();
-app.get('/',(req,res)=>{
-  if(req.url=="/") res.redirect('/login.html');
+app.get('/',(req,res)=>res.redirect('/login.html'));
+app.get('/login.html',(req,res)=>{
+  let fileName=`./public${req.url}`;
+  let fileData=getFileData(fs,fileName);
+  res.setHeader('Content-Type',getMIMEType(req.url));
+  res.write(fileData);
+  res.end();
 });
 module.exports=app;
--- test/appTest.js
+++ test/appTest.js
@@ -6,6 +6,9 @@ const should_be_redirected_to = (res,location)=>{
   assert.equal(res.statusCode,302);
   assert.equal(res.headers.location,location);
 };
+const status_is_ok = (res)=>assert.equal(res.statusCode,200);
+const content_type_is = (res,expected)=> assert.equal(res.headers['Content-Type'],expected);
+const body_contains = (res,text)=> assert.isOk(res.body.includes(text),`missing ${text}`);
 describe('app',()=>{
   describe('GET /bad',()=>{
     it('responds with 404',done=>{
@@ -20,6 +23,16 @@ describe('app',()=>{
       request(app,{method:'GET',url:'/'},(res)=>{
         should_be_redirected_to(res,'/login.html');
         assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
+  describe('GET /login.html',()=>{
+    it('gives the login page',done=>{
+      request(app,{method:'GET',url:'/login.html'},res=>{
+        status_is_ok(res);
+        content_type_is(res,'text/html');
+        body_contains(res,'LOGIN');
         done();
       })
     })
