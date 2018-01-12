60b31d7fe66ad8b12a3b5eae62a21126a35cc3cc
salman shaik <dukesalman5@gmail.com>
2018-01-12 13:59:00 +0530

    handled landing page

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let webApp=require('./webapp.js');
 let app=webApp.create();
-
+app.get('/',(req,res)=>{
+  if(req.url=="/") res.redirect('/login.html');
+});
 module.exports=app;
--- test/appTest.js
+++ test/appTest.js
@@ -2,6 +2,10 @@ let chai = require('chai');
 let assert = chai.assert;
 let app = require('../app.js');
 let request=require('./requestSimulator.js')
+const should_be_redirected_to = (res,location)=>{
+  assert.equal(res.statusCode,302);
+  assert.equal(res.headers.location,location);
+};
 describe('app',()=>{
   describe('GET /bad',()=>{
     it('responds with 404',done=>{
@@ -10,5 +14,14 @@ describe('app',()=>{
         done();
       })
     })
-  }) 
+  })
+  describe('GET /',()=>{
+    it('redirects to login.html',done=>{
+      request(app,{method:'GET',url:'/'},(res)=>{
+        should_be_redirected_to(res,'/login.html');
+        assert.equal(res.body,"");
+        done();
+      })
+    })
+  })
 })
